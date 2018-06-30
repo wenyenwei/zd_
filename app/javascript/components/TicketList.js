bb296c0776611a8deb4281ecadf9da79aa9243af
wenyenwei <wen@connecteducation.com.au>
2018-06-30 20:23:37 +1000

    react with apollo server added

---
+++ app/javascript/components/TicketList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class TicketList extends Component {
+	render(){
+		return(
+			<div>
+				<h1>Mock Up CRM Tickets</h1>
+			</div>
+		)
+
+	}
+}
+
+export default TicketList
--- app/javascript/components/Tickets.js
+++ app/javascript/components/Tickets.js
@@ -1,12 +1,27 @@
 import React from "react"
+import ReactDOM from "react-dom"
+import ApolloClient from "apollo-client"
+import { ApolloProvider } from "react-apollo"
 import PropTypes from "prop-types"
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import TicketList from './TicketList'
+
 class Tickets extends React.Component {
   render () {
+  	const cache = new InMemoryCache()
+  	const client = new ApolloClient({
+  		link: new HttpLink(),
+  		cache
+  	})
+
     return (
       <React.Fragment>
-      	<div>
-      		<h4> test title</h4>
-      	</div>
+      	<ApolloProvider client={client}>
+	      	<div>
+	      		<TicketList/>
+	      	</div>
+	    </ApolloProvider>
       </React.Fragment>
     );
   }
