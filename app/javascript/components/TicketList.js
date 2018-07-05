52e196209e050efcc367d35a33f8afb34d66c774
wenyenwei <wen@connecteducation.com.au>
2018-07-05 19:02:58 +1000

    graphql connect to react

--- app/javascript/components/TicketList.js
+++ app/javascript/components/TicketList.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react'
-import gql from 'graphql-tag'
-import { graphql } from 'react-apollo'
+// import React, { Component } from 'react'
+// import gql from 'graphql-tag'
+// import { graphql } from 'react-apollo'
 
-class TicketList extends Component {
-	render(){
-		return(
-			<div>
-				<h1>Mock Up CRM Tickets</h1>
-			</div>
-		)
+// class TicketList extends Component {
+// 	render(){
+// 		return(
+// 			<div>
+// 				<h1>Mock Up CRM Tickets</h1>
+// 				<p>{this.props.data}</p>
+// 			</div>
+// 		)
 
-	}
-}
+// 	}
+// }
 
-const query = gql`
-	{
-		tickets{
-			title
-			id
-			status
-			assignee
-			priority			
-		}
-	}
-`
+// const query = gql`
+// 	{
+// 		tickets{
+// 			title
+// 			id
+// 			status
+// 			assignee
+// 			priority			
+// 		}
+// 	}
+// `
 
-export default graphql(query)(TicketList)
+// export default graphql(query)(TicketList)
--- app/javascript/components/Tickets.js
+++ app/javascript/components/Tickets.js
@@ -1,29 +1,68 @@
 import React from "react"
-import ReactDOM from "react-dom"
-import ApolloClient from "apollo-client"
-import { ApolloProvider } from "react-apollo"
 import PropTypes from "prop-types"
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import TicketList from './TicketList'
+import ApolloClient from "apollo-boost"
+import { ApolloProvider, Query } from "react-apollo"
+import gql from "graphql-tag"
+
+
+const client = new ApolloClient({
+  uri: "/graphql"
+})
+
+client
+  .query({
+    query: gql`
+      {
+        findTicket(id: "5b3b709d875037d3b22efd57") {
+          id
+          title
+          content
+        }
+      }
+    `
+  })
+  .then(result => console.log(result))
+
+const ADD_TICKET = gql`
+      mutation addTicket($title: String!, $content: String!) {
+        addTicket(title: $title, content: $content) {
+          id
+          title
+          content
+        }
+      }
+    `
+
+const TicketData = ({ title, content }) => (
+  <Query
+    query={ ADD_TICKET }
+    variables={{ title, content }}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+      const response = data.findTicket
+      return (
+        <div key={response.id}>
+          <p>{`${response.title}: ${response.content}`}</p>
+        </div>
+      )
+    }}
+  </Query>
+)
 
 class Tickets extends React.Component {
   render () {
-  	
-  	const cache = new InMemoryCache()
-  	const client = new ApolloClient({
-  		link: new HttpLink(),
-  		cache
-  	})
-
     return (
-      <React.Fragment>
-      	<ApolloProvider client={client}>
-	      	<div>
-	      		<TicketList/>
-	      	</div>
-	    </ApolloProvider>
-      </React.Fragment>
+      <ApolloProvider client={client}>
+        <React.Fragment>
+         <div>
+           <h4>test title</h4>
+           <h5>ticket id: 5b3b709d875037d3b22efd57 title</h5>
+           <TicketData/>
+         </div>
+        </React.Fragment>
+      </ApolloProvider>
     );
   }
 }
